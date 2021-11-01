8b256bcfa3b8801d820adf7d61e3095d8a67dd4c
Hamdan <khanhamdan16200@gmail.com>
2021-11-01 14:43:39 -0400

    Removed unncessary directories and console log lines

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 //Main app
+
+//Setup Dependencies
 const path = require('path')
 const express = require('express')
 const axios = require('axios')
 const ejs = require('ejs')
-const { response } = require('express')
 
+//Configure Express
 const app = express()
 const port = process.env.PORT || 3000
 
+//Set Path for client views and assets
 const publicDir = path.join(__dirname, '../public')
 const viewsDir = path.join(__dirname, '../views')
 
@@ -16,31 +19,31 @@ app.set('views', viewsDir)
 
 app.use(express.static(publicDir))
 
+//Main Comic page
 app.get('', (req, res)=>{
-    console.log('main')
     res.redirect('/0')
 
 })
 
+//Comic page with number parameter
 app.get('/:num', (req,res)=>{
-    console.log('num', req.params.num)
     var comicNum = req.params.num
-    console.log(comicNum)
     if(!Number.isInteger(Number(comicNum))){
         return res.send('error')
     }
-
+    //Check to see if latest comic is required
     if(comicNum === '0') {
         comicNum = ''
         
     }
-    console.log('new', 'https://xkcd.com/'+comicNum+'/info.0.json')
+    //GET call from api
     axios.get('https://xkcd.com/'+comicNum+'/info.0.json').then((response) => {
         if(response.error){
             return response.error
         }
         return response.data
     }).then(({num, safe_title, day, month, year, img, alt, transcript}) => {
+        //Send retrieved data to index view
         res.status(200).render(viewsDir +'/index', {
             title: safe_title,
             day,
@@ -48,13 +51,14 @@ app.get('/:num', (req,res)=>{
             year,
             img,
             alt,
-            transcript: transcript.replace(/\n/g,'<br><br>')
+            transcript: transcript.replace(/\n/g,'<br><br>') //Replace \n with <br> tags
         })
     });
     
     
 })
 
+//404
 app.get('/*', (req, res)=>{
     return res.send(404)
 })
@@ -62,5 +66,5 @@ app.get('/*', (req, res)=>{
 
 
 app.listen(port, ()=>{
-    console.log('Server up on 3000!')
+    console.log('Server up on '+ port)
 })
